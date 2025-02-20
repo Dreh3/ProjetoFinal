727e2c32c1a53ea2f1ea3669884bbd56ac92660a
Dreh3 <andressa_sousaf@outlook.com>
2025-02-20 12:16:03 -0300

    Adicionando funções iniciais

--- controlePotencia.c
+++ controlePotencia.c
@@ -4,18 +4,48 @@
 #include "hardware/adc.h"
 
 //Definindo pinos 
-#define Led_Alerta 13
-#define LedVentilador 11
-#define EixoY 26 //??
-#define BotaoA 5
+#define Led_Alerta 13 //Simula luz de sinalização para o modo ativado
+#define LedVentilador 11 //Simula o ventilador, indicando a mudança de potência 
+#define EixoY 27 //Simula o sensor de temperatura
+#define BotaoA 5 //Habilita ou desabilita modo de verificação de temperatura
 
 //Definindo variáveis globais
-static volatile bool modohabilitado = true;
+static volatile bool modohabilitado = true; //Habilita ou desabilita modo de verificação de temperatura
 static volatile uint32_t tempo_anterior = 0; //Para o debouncing
 
 //Definindo parâmetros para o PWM
 #define DIVISOR_CLOCK 100
 #define WRAP 24999
+
+//Funções para configurar os pinos de leds e botões
+void configurar_leds(uint gpio){
+    gpio_init(gpio);
+    gpio_set_dir(gpio, GPIO_OUT);
+    gpio_put(gpio, 0);
+};
+
+void configurar_botao(uint gpio){
+    gpio_init(gpio);
+    gpio_set_dir(gpio, GPIO_IN);
+    gpio_pull_up(gpio);
+};
+
+//Função para configurar pwm
+void configurar_pwm(uint gpio){
+    uint slice;
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
+    slice = pwm_gpio_to_slice_num(gpio);
+    pwm_set_clkdiv(slice, DIVISOR_CLOCK);
+    pwm_set_wrap(slice,WRAP);
+    pwm_set_gpio_level(gpio, 0);
+    pwm_set_enabled(gpio, true);
+};
+
+//Função para mudar a potência suavemente
+void modifica_velocidade(){
+    
+};
+
 
 static void interrupcaoBotao(uint gpio, uint32_t events);
 
