61a7d7f1df8dd9197d4ae38856b921a80bdfefb8
Dreh3 <andressa_sousaf@outlook.com>
2025-02-21 10:13:03 -0300

    Concluindo código

--- controlePotencia.c
+++ controlePotencia.c
@@ -4,18 +4,25 @@
 #include "hardware/adc.h"
 
 //Definindo pinos 
-#define Led_Alerta 13 //Simula luz de sinalização para o modo ativado
+//#define Led_Alerta 13 //Simula luz de sinalização para o modo ativado
 #define LedVentilador 11 //Simula o ventilador, indicando a mudança de potência 
-#define EixoY 27 //Simula o sensor de temperatura
+#define SensorY 27 //Simula o sensor de temperatura no eixo Y do joystick
 #define BotaoA 5 //Habilita ou desabilita modo de verificação de temperatura
 
 //Definindo variáveis globais
 static volatile bool modohabilitado = true; //Habilita ou desabilita modo de verificação de temperatura
 static volatile uint32_t tempo_anterior = 0; //Para o debouncing
+uint potencia_atual = 0; //Inicia desligado
 
 //Definindo parâmetros para o PWM
 #define DIVISOR_CLOCK 100
 #define WRAP 24999
+
+//Definindo parâmetros de potência
+#define nivel0 0
+#define nivel1 500
+#define nivel2  1500
+#define nivel3  24900
 
 //Funções para configurar os pinos de leds e botões
 void configurar_leds(uint gpio){
@@ -42,8 +49,17 @@ void configurar_pwm(uint gpio){
 };
 
 //Função para mudar a potência suavemente
-void modifica_velocidade(){
-    
+void modifica_potencia(uint gpio, uint nova_potencia, uint potencia_atual){
+    bool incrementa = (nova_potencia>potencia_atual)?true:false;
+    while(potencia_atual!=nova_potencia){
+        if(incrementa){
+            potencia_atual += 100;
+        }else{
+            potencia_atual -= 100;
+        };
+        pwm_set_gpio_level(gpio, potencia_atual);
+        sleep_ms(10);
+    };
 };
 
 
@@ -51,15 +67,50 @@ static void interrupcaoBotao(uint gpio, uint32_t events);
 
 int main()
 {
+    uint nivel_atual = nivel0;
+    uint nivel_anterior = nivel0;
+    uint temperatura = 20;
+
     stdio_init_all();
+    adc_init();
+
+     //Inicializando entradas analógicas
+     adc_gpio_init(SensorY);
+
+    configurar_leds(LedVentilador); //Já inicia desligado
+    //configurar_leds(LedAlerta);
+    configurar_botao(BotaoA);
+    configurar_pwm(LedVentilador);
 
     //Chamada da interrupção para o botão
     gpio_set_irq_enabled_with_callback(BotaoA, GPIO_IRQ_EDGE_FALL, true, interrupcaoBotao);
 
     while (true) {
-        
-    }
-}
+
+        if(modohabilitado){
+            //Leitura do eixo Y para pegar dados analógicos que simulam o sensor de temperatura
+            //Relação que converte o valor lido na entrada analalógica para temperatura na faixa de 20° a 35°
+            adc_select_input(0);
+            temperatura = (15*adc_read() + 20 * 4096)/4096;
+
+            //Condicionais para identificar os intervalos correpondentes às potências
+            if(temperatura<=20){
+                nivel_anterior = nivel_atual;
+                nivel_atual = nivel0;
+            }else if(temperatura>20 && temperatura<=25){
+                nivel_anterior = nivel_atual;
+                nivel_atual = nivel1;
+            }else if(temperatura>25 && temperatura<=30){
+                nivel_anterior = nivel_atual;
+                nivel_atual = nivel2;
+            }else if(temperatura>30){
+                nivel_anterior = nivel_atual;
+                nivel_atual = nivel3;
+            };
+            modifica_potencia(LedVentilador, nivel_atual, nivel_anterior);
+        };
+    };
+};
 
 
 //Função de interrupção para habilitar/desabilita funcionalidade a partir do Botão A
